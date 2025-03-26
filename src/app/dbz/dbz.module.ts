2ad75daed4ebcfe4b2a493937705f08f46322b87
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-26 17:23:03 -0500

    Update: DbzModule

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { dbzComponent } from './pages/main-page.component';
+import { ListComponent } from "./components/list/list.component";
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
-  declarations: [dbzComponent],
+  declarations: [
+    dbzComponent,
+    ListComponent,
+    AddCharacterComponent],
   imports: [
     CommonModule
-  ],
+],
   exports: [
     dbzComponent
   ]
