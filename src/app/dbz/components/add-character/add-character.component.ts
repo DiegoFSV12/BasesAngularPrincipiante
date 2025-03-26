86af9a23ed876bab14bf561740a80c10ac39b0ff
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-26 17:23:26 -0500

    feat: Add add-character.component

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrl: './add-character.component.css',
+  standalone: false
+})
+export class AddCharacterComponent { }
