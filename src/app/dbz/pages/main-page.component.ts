ec4aea3b838d05ea6b49200b82df26f830368648
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-27 16:24:52 -0500

    Update: dbzComponent

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -10,9 +10,11 @@ import { Character } from "../interfaces/character.interface";
 export class dbzComponent {
     public characters:Character[] = [{
         name:'Goku',
-        power:50000
+        power:50000,
+        src:"https://preview.redd.it/anyone-else-want-skibidi-goku-in-the-game-v0-915gumkk0pad1.jpg?width=224&format=pjpg&auto=webp&s=9eac03d3439013133779af8aa6a94a0ba332079a"
     },{
         name:'Vegeta',
-        power:40000
+        power:40000,
+        src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSfdmRr9o5XvjHeDpuknHYzZvEqdfi6SI_Kg&s"
     }];
 }
