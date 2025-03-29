0e29be9b0b251456a3cc54a477e49e4f240c71da
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-28 21:24:33 -0500

    Update: addCharacterComponent

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -8,6 +8,10 @@ import { Character } from '../../interfaces/character.interface';
   standalone: false
 })
 export class AddCharacterComponent {
+
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter;
+
   public character:Character = {
     name:'',
     power:0,
@@ -15,5 +19,8 @@ export class AddCharacterComponent {
   }
   addCharacter():void{
     console.log(this.character);
+    if(this.character.name.length === 0 || this.character.src.length === 0) return;
+    this.onNewCharacter.emit(this.character);
+    this.character={name:'',power:0,src:''};
   }
 }
